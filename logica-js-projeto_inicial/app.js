2cebc11754fdbefcd61277511adeeda841fd660e
Gabriel Brito <dosgabrielbrito@gmail.com>
2024-05-20 17:38:23 -0300

    Aula 01

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,35 @@
+//Aula:
+
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 29;
+let numeroUsuario = prompt('Escolha um número entre 1 e 30');
+
+if (numeroUsuario == numeroSecreto) {
+    console.log('Isso aí! Você descobriu o número secreto (29)');
+}
+
+//Exercícios:
+
+// alert('Boas vindas ao nosso site!');
+
+// let nome = 'Lua';
+
+// let idade = 25;
+
+// let numeroDeVendas = 50;
+
+// let saldoDisponivel = 1000;
+
+// alert('Erro! Preencha todos os campos');
+
+// let mensagemErro = 'Erro! Preencha todos os campos';
+
+// alert(mensagemErro);
+
+// nome = prompt('Qual o seu nome?');
+
+// idade = prompt('Qual a sua idade?');
+
+// if (idade >= 18) {
+//     alert('Já pode tirar sua habilitação!')
+// }
